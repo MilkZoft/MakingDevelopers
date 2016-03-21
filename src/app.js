1dbe0f9cf78505e723becaa06b30345460c347e4
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-03-20 23:49:05 -0700

    V2 - Moving project to src folder & adding pm2

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+console.log('Hola mundo');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -56,5 +56,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+if (!module.parent) {
+  app.listen(3333);
+}
 
 module.exports = app;
