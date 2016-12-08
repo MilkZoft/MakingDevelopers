92a2bd5981405169db55d12d66a152ab16a2a337
Carlos Santana <csantana@beachbody.com>
2016-12-07 18:33:57 -0800

    Adding Unit Tests for Files

--- test/lib/utils/filesTest.js
+++ test/lib/utils/filesTest.js
@@ -13,17 +13,63 @@ describe('@Files', () => {
     it('should be a function', () => {
       assert.typeOf(getFileExtension, 'function', 'getFileExtension should be a function');
     });
+
+    it('should return the file extension', () => {
+      const testData = 'foo.jpg';
+      const expectedResult = 'jpg';
+      const actualResult = getFileExtension(testData);
+
+      assert.isTrue(actualResult === expectedResult, 'actualResult should be jpg');
+    });
   });
 
   describe('#getFileFormats', () => {
     it('should be a function', () => {
       assert.typeOf(getFileFormats, 'function', 'getFileFormats should be a function');
     });
+
+    it('should return the file formats', () => {
+      const expectedResult = {
+        'pdf': 'pdf',
+        'docx': 'word',
+        'js': 'code',
+        'json': 'code',
+        'mp4': 'video',
+        'rar': 'zip',
+        'sql': 'code',
+        'zip': 'zip'
+      };
+
+      const actualResult = getFileFormats();
+
+      assert.deepEqual(
+        actualResult,
+        expectedResult,
+        'actualResult should match expectedResult'
+      );
+    });
   });
 
   describe('#getFileInfo', () => {
     it('should be a function', () => {
       assert.typeOf(getFileInfo, 'function', 'getFileInfo should be a function');
+    });
+
+    it('should return an array with the file info', () => {
+      const testData = 'test/foo.png';
+
+      const expectedResult = {
+        name: 'foo',
+        extension: 'png'
+      };
+
+      const actualResult = getFileInfo(testData);
+
+      assert.deepEqual(
+        actualResult,
+        expectedResult,
+        'actualResult should match expectedResult'
+      );
     });
   });
 
@@ -36,6 +82,17 @@ describe('@Files', () => {
   describe('#getImageFormats', () => {
     it('should be a function', () => {
       assert.typeOf(getImageFormats, 'function', 'getImageFormats should be a function');
+    });
+
+    it('should return the image formats', () => {
+      const expectedResult = ['png', 'jpg', 'jpeg', 'gif'];
+      const actualResult = getImageFormats();
+
+      assert.deepEqual(
+        actualResult,
+        expectedResult,
+        'actualResult should match expectedResult'
+      );
     });
   });
 
