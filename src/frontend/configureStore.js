b0aa7f6364239c522ed6a04cfcb8b8376476b5fc
Carlos Santana <csantana@beachbody.com>
2017-02-24 16:23:16 -0800

    Adding Isomorphic Fetch Support

--- src/frontend/configureStore.js
+++ src/frontend/configureStore.js
@@ -1,19 +1,34 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+// Dependencies
+import isomorphicFetch from 'isomorphic-fetch';
 import promiseMiddleware from 'redux-promise-middleware';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import { createStore, applyMiddleware } from 'redux';
+
+// Reducers
+import rootReducer from './reducers';
+
+const injectMiddleware = deps => ({ dispatch, getState }) => next => action =>
+  next(typeof action === 'function'
+    ? action({ ...deps, dispatch, getState })
+    : action
+  );
 
 export default function configureStore(options) {
   const { initialState = {} } = options;
 
+  const middleware = [
+    injectMiddleware({
+      fetch: isomorphicFetch
+    }),
+    promiseMiddleware({
+      promiseTypeSuffixes: ['START', 'SUCCESS', 'ERROR']
+    }),
+    reduxImmutableStateInvariant()
+  ];
+
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(
-      reduxImmutableStateInvariant(),
-      promiseMiddleware({
-        promiseTypeSuffixes: ['START', 'SUCCESS', 'ERROR']
-      })
-    )
+    applyMiddleware(...middleware)
   );
 }
